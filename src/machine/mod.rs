4723e8f721d8420d71fb52b4af251ec843e89601
schukark <schukark2004@gmail.com>
2025-09-29 01:37:01 +0300

    feat: added Chip8 structure managing all its components

---
+++ src/machine/mod.rs
@@ -0,0 +1,299 @@
+//! Chip8 machine
+//!
+//! Contains all components of chip8: registers, stack, display, timers and sounds
+
+use thiserror::Error;
+
+use crate::{
+    decoder::instruction::{DecodeError, Instruction},
+    machine::{
+        cpu::{Cpu, CpuError},
+        display::{Display, DisplayError},
+        keypad::{Keypad, KeypadError},
+        memory::{Memory, MemoryError},
+    },
+    types::Index,
+};
+
+mod cpu;
+mod display;
+mod keypad;
+mod memory;
+
+/// Enum of program counter statuses after command execution
+enum ExecResult {
+    /// PC should advance to next word
+    Advance,
+    /// Last instruction jumped, no need to move PC
+    Jumped,
+    /// Waiting on something, no need to move PC
+    Wait,
+}
+
+/// Full Chip-8 machine
+pub struct Chip8 {
+    /// Chip8 cpu
+    pub cpu: Cpu,
+    /// Chip8 memory
+    pub memory: Memory,
+    /// Chip8 display
+    pub display: Display,
+    /// Chip8 keypad
+    pub keypad: Keypad,
+}
+
+#[derive(Debug, Error)]
+/// Enum of all possible errors with chip8 instance
+pub enum Chip8Error {
+    #[error("Cpu error")]
+    /// Cpu error
+    CpuError(#[from] CpuError),
+    #[error("Memory error")]
+    /// Memory error
+    MemoryError(#[from] MemoryError),
+    #[error("Display error")]
+    /// Display error
+    DisplayError(#[from] DisplayError),
+    #[error("Ram error")]
+    /// Keypad error
+    KeypadError(#[from] KeypadError),
+    #[error("Decode error")]
+    /// Instruction decoding error
+    DecodeError(#[from] DecodeError),
+    #[error("Unsupported instruction")]
+    /// Unsupported instruction called (assembly subroutines)
+    UnsupportedInstruction,
+}
+
+impl Chip8 {
+    /// Create a new Chip8
+    pub fn new() -> Self {
+        Self {
+            cpu: Cpu::new(),
+            memory: Memory::new(),
+            display: Display::new(),
+            keypad: Keypad::new(),
+        }
+    }
+
+    /// Load the program for execution, starting at address 0x200
+    pub fn load_program(&mut self, program: &[u8]) -> Result<(), Chip8Error> {
+        self.memory.load(0x200, program);
+        self.cpu.set_program_counter(0x200)?;
+
+        Ok(())
+    }
+
+    /// Run one fetch-decode-execute cycle
+    pub fn step(&mut self) -> Result<(), Chip8Error> {
+        let pc = self.cpu.get_program_counter();
+        let opcode = self.memory.read_word(pc);
+        let instruction = Instruction::try_from(opcode)?;
+
+        match self.execute(instruction) {
+            Ok(ExecResult::Advance) => self.cpu.advance_program_counter(2)?,
+            Ok(ExecResult::Jumped) => {}
+            Ok(ExecResult::Wait) => {}
+            Err(err) => return Err(err),
+        };
+
+        Ok(())
+    }
+
+    /// Execute an instruction
+    fn execute(&mut self, instruction: Instruction) -> Result<ExecResult, Chip8Error> {
+        let result = match instruction {
+            Instruction::CallMachineCode { address: _address } => {
+                return Err(Chip8Error::UnsupportedInstruction);
+            }
+            Instruction::ClearDisplay => {
+                self.display.clear();
+                ExecResult::Advance
+            }
+            Instruction::Return => {
+                let new_pc = self.cpu.stack_pop()?;
+                self.cpu.set_program_counter(new_pc)?;
+                ExecResult::Jumped
+            }
+            Instruction::Goto { address } => {
+                self.cpu.set_program_counter(address.into_inner())?;
+                ExecResult::Jumped
+            }
+            Instruction::CallSubroutine { address } => {
+                self.cpu.stack_push(self.cpu.get_program_counter())?;
+                self.cpu.set_program_counter(address.into_inner())?;
+                ExecResult::Jumped
+            }
+            Instruction::EqConst { x, value } => {
+                if *self.cpu.vx(x) == value {
+                    self.cpu.advance_program_counter(2)?;
+                }
+
+                ExecResult::Advance
+            }
+            Instruction::NeqConst { x, value } => {
+                if *self.cpu.vx(x) != value {
+                    self.cpu.advance_program_counter(2)?;
+                }
+                ExecResult::Advance
+            }
+            Instruction::EqReg { x, y } => {
+                let vx = *self.cpu.vx(x);
+                let vy = *self.cpu.vx(y);
+                if vx == vy {
+                    self.cpu.advance_program_counter(2)?;
+                }
+                ExecResult::Advance
+            }
+            Instruction::AssignMem { x, value } => {
+                *self.cpu.vx(x) = value;
+                ExecResult::Advance
+            }
+            Instruction::AddAssignMem { x, value } => {
+                *self.cpu.vx(x) += value;
+                ExecResult::Advance
+            }
+            Instruction::AssignReg { x, y } => {
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) = vy;
+                ExecResult::Advance
+            }
+            Instruction::OrReg { x, y } => {
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) |= vy;
+                ExecResult::Advance
+            }
+            Instruction::AndReg { x, y } => {
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) &= vy;
+                ExecResult::Advance
+            }
+            Instruction::XorReg { x, y } => {
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) ^= vy;
+                ExecResult::Advance
+            }
+            Instruction::AddAssignReg { x, y } => {
+                let vx = *self.cpu.vx(x);
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) += vy;
+                *self.cpu.vx(Index::try_new(0xF).unwrap()) =
+                    ((vx as u16) + (vy as u16) > 255) as u8;
+                ExecResult::Advance
+            }
+            Instruction::SubAssignReg { x, y } => {
+                let vx = *self.cpu.vx(x);
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) -= vy;
+                *self.cpu.vx(Index::try_new(0xF).unwrap()) = (vx >= vy) as u8;
+                ExecResult::Advance
+            }
+            Instruction::RShift { x, y: _y } => {
+                *self.cpu.vx(Index::try_new(0xF).unwrap()) = *self.cpu.vx(x) & 1;
+                *self.cpu.vx(x) >>= 1;
+                ExecResult::Advance
+            }
+            Instruction::SubAssignRegInverse { x, y } => {
+                let vx = *self.cpu.vx(x);
+                let vy = *self.cpu.vx(y);
+                *self.cpu.vx(x) = vy - vx;
+                *self.cpu.vx(Index::try_new(0xF).unwrap()) = (vy >= vx) as u8;
+                ExecResult::Advance
+            }
+            Instruction::LShift { x, y: _y } => {
+                *self.cpu.vx(Index::try_new(0xF).unwrap()) = (*self.cpu.vx(x) >> 7) & 1;
+                *self.cpu.vx(x) <<= 1;
+                ExecResult::Advance
+            }
+            Instruction::NeqReg { x, y } => {
+                let vx = *self.cpu.vx(x);
+                let vy = *self.cpu.vx(y);
+                if vx != vy {
+                    self.cpu.advance_program_counter(2)?;
+                }
+                ExecResult::Advance
+            }
+            Instruction::SetI { address } => {
+                self.cpu.set_address(address.into_inner())?;
+                ExecResult::Advance
+            }
+            Instruction::GotoPlusV0 { address } => {
+                let vx = *self.cpu.vx(Index::try_new(0x0_u8).unwrap());
+                self.cpu
+                    .set_program_counter(address.into_inner() + vx as u16)?;
+                ExecResult::Jumped
+            }
+            Instruction::Rand { x, value } => {
+                let rand = rand::random::<u8>();
+                *self.cpu.vx(x) = rand & value;
+                ExecResult::Advance
+            }
+            Instruction::DrawSprite {
+                x: _,
+                y: _,
+                height: _,
+            } => {
+                unimplemented!("Display is not yet implemented")
+            }
+            Instruction::KeyPressedSkip { x: _ } => {
+                unimplemented!("Keypad is not yet implemented")
+            }
+            Instruction::KeyReleasedSkip { x: _ } => {
+                unimplemented!("Keypad is not yet implemented")
+            }
+            Instruction::GetDelayTimer { x } => {
+                *self.cpu.vx(x) = self.cpu.get_delay_timer();
+                ExecResult::Advance
+            }
+            Instruction::AwaitKeyPress { x: _ } => unimplemented!("Keypad is not yet implemented"),
+            Instruction::SetDelayTimer { x } => {
+                let vx = *self.cpu.vx(x);
+                self.cpu.set_delay_timer(vx);
+                ExecResult::Advance
+            }
+            Instruction::SetSoundTimer { x } => {
+                let vx = *self.cpu.vx(x);
+                self.cpu.set_sound_timer(vx);
+                ExecResult::Advance
+            }
+            Instruction::AddAssignAddress { x } => {
+                let vx = *self.cpu.vx(x);
+
+                self.cpu.advance_address(vx as u16)?;
+                ExecResult::Advance
+            }
+            Instruction::SetSpriteAddr { x: _ } => {
+                unimplemented!("Display is not yet implemented")
+            }
+            Instruction::SetBCD { x } => {
+                let vx = *self.cpu.vx(x);
+
+                let hundreds = vx / 100;
+                let tens = (vx / 10) % 10;
+                let ones = vx % 10;
+
+                self.memory
+                    .load(self.cpu.get_address(), &[hundreds, tens, ones]);
+                ExecResult::Advance
+            }
+            Instruction::DumpRegisters { x } => {
+                for i in 0..=x.into_inner() {
+                    self.memory.load(
+                        self.cpu.get_address() + i as u16,
+                        &[*self.cpu.vx(Index::try_new(i).unwrap())],
+                    );
+                }
+                ExecResult::Advance
+            }
+            Instruction::LoadRegisters { x } => {
+                for i in 0..=x.into_inner() {
+                    *self.cpu.vx(Index::try_new(i).unwrap()) =
+                        self.memory.read_byte(self.cpu.get_address() + i as u16);
+                }
+                ExecResult::Advance
+            }
+        };
+
+        Ok(result)
+    }
+}
