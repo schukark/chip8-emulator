8db73591c3c73c1f705c1ce4c9213e0c57f5ff79
schukark <schukark2004@gmail.com>
2025-09-29 19:56:56 +0300

    feat: added draw_sprite method for Display

--- src/machine/display.rs
+++ src/machine/display.rs
@@ -27,4 +27,24 @@ impl Display {
     pub fn clear(&mut self) {
         self.pixels = [[false; 64]; 32];
     }
+
+    /// Draw the sprite starting at (vx, vy)
+    ///
+    /// Returns true if any pixels were turned off by drawing this sprite
+    pub fn draw_sprite(&mut self, sprite: &[u8], vx: u8, vy: u8) -> bool {
+        let mut collision = false;
+
+        for (idx, word) in sprite.iter().enumerate() {
+            for i in 0..8 {
+                let pixel = (word >> i) & 0x1;
+                let pos_x = (vx as usize + i) % 64;
+                let pos_y = (vy as usize + idx) % 32;
+                if self.pixels[pos_y][pos_x] {
+                    collision = true;
+                }
+                self.pixels[pos_y][pos_x] ^= pixel == 1;
+            }
+        }
+        collision
+    }
 }
