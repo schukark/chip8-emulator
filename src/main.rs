afa73e0f6940c676a454d7941bd7882973e95b2d
schukark <schukark2004@gmail.com>
2025-10-01 01:30:35 +0300

    feat: setup logging and made enumaltor game loop skeleton

--- src/main.rs
+++ src/main.rs
@@ -4,51 +4,45 @@
 #![warn(clippy::missing_docs_in_private_items)]
 #![feature(custom_test_frameworks)]
 
-use std::{env::args, fs::File, io::Read, thread::sleep, time::Duration};
-
-use crate::{decoder::instruction::decode, machine::Chip8};
-
 mod decoder;
+mod game_loop;
 mod machine;
 mod types;
 
+use tklog::{Format, LEVEL, LOG};
+
+/// Setup logging
+fn log_init() {
+    LOG.set_console(false) // Enables console logging
+        .set_level(LEVEL::Trace) // Sets the log level; default is Debug
+        .set_format(Format::LevelFlag | Format::Time | Format::ShortFileName) // Defines structured log output with chosen details
+        .set_cutmode_by_size("tklogsize.txt", 1 << 20, 10, true) // Cuts logs by file size (1 MB), keeps 10 backups, compresses backups
+        .set_formatter("{level}{time} {file}:{message}\n"); // Customizes log output format; default is "{level}{time} {file}:{message}"
+}
+
 fn main() {
-    let mut args = args();
+    let args = std::env::args();
+    log_init();
 
     if args.len() != 2 {
-        eprintln!("Usage: chip8-emulator path/to/rom");
+        eprintln!("Usage: chip8-emulator /path/to/your/rom");
         return;
     }
 
-    let mut file = File::open(args.nth(1).unwrap()).expect("The provided rom file should exist");
-    let mut buf = Vec::new();
-    file.read_to_end(&mut buf).expect("Couldn't read all file");
+    let program_path = std::env::args().nth(1).unwrap();
+    let program = std::fs::read(program_path).expect("Error occured when opening rom");
 
-    let decoded = decode(&buf);
-
-    let decoded = match decoded {
+    let mut chip8 = match game_loop::load_program(&program) {
+        Ok(x) => x,
         Err(e) => {
-            eprintln!("An error occured: {e}");
+            eprintln!("An error occured when loading the program, error: {e}");
             return;
         }
-        Ok(decoded_instructions) => decoded_instructions,
     };
-
-    for instruction in decoded {
-        println!("{}", instruction);
-    }
-
-    let mut chip8 = Chip8::new();
-    if let Err(e) = chip8.load_program(&buf) {
-        eprintln!("Error occured in chip8 emulation: {e}");
-    }
-
-    loop {
-        if let Err(e) = chip8.step() {
-            eprintln!("Error occured in chip8 emulation: {e}");
-            return;
+    match game_loop::run_game(&mut chip8) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("Error occured during the game {e}");
         }
-
-        sleep(Duration::from_secs(1));
     }
 }
